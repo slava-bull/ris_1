5d9f589ed75983f568190b9ddff871f6bdc03a1d
Slava-bull <slava-bull@mail.ru>
2021-02-11 11:32:10 +0700

    Логирование hello world

--- src/main/java/com/slava_bull/first_task/EntryPoint.java
+++ src/main/java/com/slava_bull/first_task/EntryPoint.java
@@ -1,7 +1,13 @@
 package com.slava_bull.first_task;
 
+import org.apache.log4j.Logger;
+
 public class EntryPoint {
+
+    final static Logger log = Logger.getLogger(EntryPoint.class.getName());
+
     public static void main(String[] args) {
+        log.debug("Log: hello world");
         System.out.println("Hello world");
     }
 }
